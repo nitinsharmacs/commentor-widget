ef214baeffd9859cd90475cc978619373bcf3a16
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2023-03-23 18:30:08 +0530

    Create webpack production config

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,36 +1,24 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const plugins = [new MiniCssExtractPlugin({filename: 'commentor.css'})];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.tsx',
+  },
   output: {
-    filename: 'static/js/[name].[contenthash].bundle.js',
-    chunkFilename: 'static/js/chunk.[contenthash:8].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'commentor.js',
+    path: path.resolve('./build'),
     clean: true,
     publicPath: '',
   },
   resolve: {
-    extensions: ['.jsx', '.js'],
+    extensions: ['.wasm', '.ts', '.tsx', '.mjs', '.cjs', '.js', '.json'],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      minify: true,
-      template: './public/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'static/css/[name].[contenthash].bundle.css',
-    }),
-  ],
+  plugins,
   mode: 'production',
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      maxSize: 1024 * 50, // 3kb
-    },
-  },
+  optimization: {},
   module: {
     rules: [
       {
@@ -44,24 +32,43 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: 'ts-loader',
+        },
+        exclude: /node_modules/,
       },
       {
-        test: /\.(jpeg)$/,
-        type: 'asset/resource',
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
-        test: /\.csv$/,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.txt$/,
-        type: 'asset/source',
+        test: /\.(svg|png|jpe?g|gif|webp)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
